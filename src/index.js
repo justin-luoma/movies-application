c76279b14d486ef378b4a55189ffe60b32f06b81
Justin Luoma <justin@justinluoma.com>
2018-09-12 15:54:39 -0500

    Initial commit, movie list element implemented

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
 /**
  * es6 modules and imports
  */
-import sayHello from './hello';
-sayHello('World');
-
 /**
  * require style imports
  */
 const {getMovies} = require('./api.js');
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
+const clickHandler = event => {
+    if (event.target.classList.contains('movie-delete-btn')) {
+        console.log(event.target.dataset.target);
+    }
+};
+
+const createTable = movies => {
+    const tBody = document.getElementById('movie-tbl');
+    let tBodyHtml = "";
+    for(const {title, rating, id} of movies) {
+        tBodyHtml +=`<tr>
+                        <td class="pl-1">${title}</td>
+                        <td class="pl-1">${rating}</td>
+                        <td class="pr-1">
+                            <a class="btn-floating btn-small waves-effect waves-light red right">
+                                <i data-target="${id}" class="material-icons movie-delete-btn">delete</i>
+                            </a>
+                        </td>
+                    </tr>`
+    }
+    tBody.innerHTML = tBodyHtml;
+    document.getElementById('loading').classList.add('hide');
+    document.getElementById('movies').classList.remove('hide');
+};
+
+getMovies().then(createTable).catch((error) => {
+    console.log(error);
 });
+
+document.addEventListener('click', clickHandler);
